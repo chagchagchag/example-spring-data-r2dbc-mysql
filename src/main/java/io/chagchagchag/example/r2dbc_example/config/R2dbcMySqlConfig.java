26ea9667b97c8caa3d54aa8cc8759f157709b6e7
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-01 20:50:56 +0900

    R2dbcMySqlConfig

---
+++ src/main/java/io/chagchagchag/example/r2dbc_example/config/R2dbcMySqlConfig.java
@@ -0,0 +1,41 @@
+package io.chagchagchag.example.r2dbc_example.config;
+
+import io.asyncer.r2dbc.mysql.MySqlConnectionConfiguration;
+import io.asyncer.r2dbc.mysql.MySqlConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+@Configuration
+public class R2dbcMySqlConfig {
+
+  @Bean
+  public MySqlConnectionFactory mySqlConnectionFactory(){
+    MySqlConnectionConfiguration config = MySqlConnectionConfiguration.builder()
+        .host("localhost")
+        .port(23306)
+        .username("user").password("test1357")
+        .database("example")
+        .build();
+
+    return MySqlConnectionFactory.from(config);
+  }
+
+  @Bean
+  public ReactiveTransactionManager transactionManager(MySqlConnectionFactory connectionFactory){
+    return new R2dbcTransactionManager(connectionFactory);
+  }
+
+  @Bean
+  public ConnectionFactoryInitializer initializer(MySqlConnectionFactory connectionFactory){
+    ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+    initializer.setConnectionFactory(connectionFactory);
+    ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+    initializer.setDatabasePopulator(populator);
+    return initializer;
+  }
+
+}
